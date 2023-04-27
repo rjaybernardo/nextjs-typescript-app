9c778a364dd25946e1b64162f657fb838e34ebae
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-04-28 06:38:11 +0800

    add functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,15 +16,24 @@ type User = {
 
 const Home = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch("https://reqres.in/api/users");
+        const response = await fetch(
+          `https://reqres.in/api/users?per_page=6&page=${page}`
+        );
         const data = await response.json();
 
         if (response.ok) {
-          setUsers(data.data);
+          if (page === 1) {
+            setUsers(data.data);
+          } else {
+            setUsers((prevUsers) => [...prevUsers, ...data.data]);
+          }
+          setTotalPages(data.total_pages);
         } else {
           console.error("Failed to fetch users:", data.error);
         }
@@ -34,10 +43,26 @@ const Home = () => {
     };
 
     fetchUsers();
-  }, []);
+  }, [page]);
+
+  const handleLoadMore = () => {
+    if (page < totalPages) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const buttonClass =
+    page === totalPages ? `${styles.btn} ${styles.disabled}` : styles.btn;
 
   return (
-    <div className={`${styles.container}   ${inter.className}`}>
+    <div className={`${styles.container} ${inter.className}`}>
+      <Head>
+        <title>My Next.js App</title>
+        <meta
+          name="description"
+          content="This is my Next.js app for demonstration purposes."
+        />
+      </Head>
       <div className={styles.grid}>
         {users.map((user) => (
           <div key={user.id} className={styles.card}>
@@ -58,7 +83,13 @@ const Home = () => {
           </div>
         ))}
       </div>
-      <button className={styles.btn}>Load More</button>
+      <button
+        onClick={handleLoadMore}
+        className={buttonClass}
+        disabled={page === totalPages}
+      >
+        Load More
+      </button>
     </div>
   );
 };
